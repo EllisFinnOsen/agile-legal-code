f8eccae574d7799152fe943ff99aa859554d6645
Ellis Osen <timothy.osen@gmail.com>
2024-09-14 17:44:14 -0500

    Changed videos to native webflow players

--- loadVideo.js
+++ loadVideo.js
@@ -1,30 +1,53 @@
-function delayedLoadVideo() {
-    var desktopWrapper = document.getElementById('desktop-goes-here');
-    var mobileWrapper = document.getElementById('mobile-goes-here');
+function manageBackgroundVideo() {
+    var desktopVideoWrapper = document.getElementById('desktop-goes-here');
+    var mobileVideoWrapper = document.getElementById('mobile-goes-here');
     var viewportWidth = window.innerWidth;
 
-    // Function to load the desktop video
+    // Remove any existing video and log the action
+    function clearVideo(wrapper, wrapperName) {
+        var videoElement = wrapper.querySelector('video');
+        if (videoElement) {
+            wrapper.innerHTML = ''; // Remove the video to reduce load
+            console.log(`${wrapperName} video cleared.`);
+        }
+    }
+
+    // Add video if not already present and log the action
     function loadDesktopVideo() {
-        if (!desktopWrapper.innerHTML) {
-            desktopWrapper.innerHTML = '<iframe src="https://player.vimeo.com/video/1009508858?background=1&amp;quality=1080p" width="100%" height="100%" frameborder="0" allow="autoplay; fullscreen" allowfullscreen=""></iframe>';
+        if (!desktopVideoWrapper.querySelector('video')) {
+            desktopVideoWrapper.innerHTML = `
+                <video autoplay loop muted playsinline style="background-image:url('https://cdn.prod.website-files.com/66d8ae0d82991ca9e4fb3c31%2F66e602a1965fc589ef0d9b5a_Landscape%20Agile%20Legal-poster-00001.jpg')" data-object-fit="cover">
+                    <source src="https://cdn.prod.website-files.com/66d8ae0d82991ca9e4fb3c31%2F66e602a1965fc589ef0d9b5a_Landscape%20Agile%20Legal-transcode.mp4" type="video/mp4">
+                    <source src="https://cdn.prod.website-files.com/66d8ae0d82991ca9e4fb3c31%2F66e602a1965fc589ef0d9b5a_Landscape%20Agile%20Legal-transcode.webm" type="video/webm">
+                </video>`;
+            console.log("Desktop video loaded.");
         }
     }
 
-    // Function to load the mobile video
     function loadMobileVideo() {
-        if (!mobileWrapper.innerHTML) {
-            mobileWrapper.innerHTML = '<iframe src="https://player.vimeo.com/video/1009508905?background=1&amp;quality=1080p" width="100%" height="100%" frameborder="0" allow="autoplay; fullscreen" allowfullscreen=""></iframe>';
+        if (!mobileVideoWrapper.querySelector('video')) {
+            mobileVideoWrapper.innerHTML = `
+                <video autoplay loop muted playsinline style="background-image:url('https://cdn.prod.website-files.com/66d8ae0d82991ca9e4fb3c31%2F66e602f236084642bf0cfd16_Portrait%20Agile%20Legal-poster-00001.jpg')" data-object-fit="cover">
+                    <source src="https://cdn.prod.website-files.com/66d8ae0d82991ca9e4fb3c31%2F66e602f236084642bf0cfd16_Portrait%20Agile%20Legal-transcode.mp4" type="video/mp4">
+                    <source src="https://cdn.prod.website-files.com/66d8ae0d82991ca9e4fb3c31%2F66e602f236084642bf0cfd16_Portrait%20Agile%20Legal-transcode.webm" type="video/webm">
+                </video>`;
+            console.log("Mobile video loaded.");
         }
     }
 
-    // Delay loading by 5 seconds
-    setTimeout(function() {
-        if (viewportWidth > 767) {
-            loadDesktopVideo();
-        } else {
-            loadMobileVideo();
-        }
-    }, 2250); // 5-second delay
+    // Log viewport width
+    console.log(`Viewport width: ${viewportWidth}`);
+
+    // Handle which video to load based on screen size
+    if (viewportWidth > 767) { // Desktop and larger
+        loadDesktopVideo();
+        clearVideo(mobileVideoWrapper, "Mobile");
+    } else { // Mobile and smaller
+        loadMobileVideo();
+        clearVideo(desktopVideoWrapper, "Desktop");
+    }
 }
 
-window.addEventListener('load', delayedLoadVideo);
+// Trigger on page load and window resize
+window.addEventListener('load', manageBackgroundVideo);
+window.addEventListener('resize', manageBackgroundVideo);
