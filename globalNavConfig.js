8ee4619bfa7cf46e27dc3244f1e84066778eb10e
Ellis Osen <timothy.osen@gmail.com>
2024-09-15 00:48:49 -0500

    Edits to globalnavjs

--- globalNavConfig.js
+++ globalNavConfig.js
@@ -8,12 +8,17 @@ waitless.scripts.push(
 
 // This function is the callback that will run after your script has been dynamically loaded
 function customScriptLoaded() {
+    console.log('Custom script loaded and callback is triggered.');
+
     jQuery(function() {
+        console.log('jQuery is ready.');
+
         let lastScrollY = 0;
         let scheduledAnimationFrame = false;
 
         // Reinitialize Webflow interactions
         function reinitializeWebflowInteractions() {
+            console.log('Reinitializing Webflow interactions.');
             requestAnimationFrame(() => {
                 Webflow.require('ix2').init();
             });
@@ -21,6 +26,7 @@ function customScriptLoaded() {
 
         // Replace dropdowns with a static link if there's only one subservice
         function handleSingleSubservice() {
+            console.log('Handling single subservice dropdowns.');
             jQuery('.service-drop-down').each(function() {
                 const $dropdown = jQuery(this);
                 const $subserviceList = $dropdown.find('.subservice-list');
@@ -30,6 +36,8 @@ function customScriptLoaded() {
                 if ($subservices.length === 1) {
                     const singleSubserviceUrl = $subservices.attr('href');
                     const singleSubserviceText = $subservices.find('.button-text').text();
+
+                    console.log(`Single subservice found: ${singleSubserviceText}, URL: ${singleSubserviceUrl}`);
 
                     if (singleSubserviceUrl && singleSubserviceUrl !== "#") {
                         const staticLinkHtml = `
@@ -52,6 +60,7 @@ function customScriptLoaded() {
 
         // Update the corresponding footer link
         function updateFooterLink(serviceText, serviceUrl) {
+            console.log(`Updating footer link: ${serviceText}, URL: ${serviceUrl}`);
             jQuery('.underline-link.is-footer').each(function() {
                 const $footerLink = jQuery(this);
                 const footerLinkText = $footerLink.find('.button-text-2').text().trim();
@@ -64,6 +73,7 @@ function customScriptLoaded() {
 
         // Add "View all {Area Name}" link after subservice lists are loaded
         function addViewAllLink(serviceAreaName, serviceAreaUrl, $container) {
+            console.log(`Adding "View all" link for ${serviceAreaName}.`);
             const viewAllText = serviceAreaName.endsWith('Services') ? serviceAreaName : `${serviceAreaName} Services`;
 
             const viewAllLinkHtml = `
@@ -80,13 +90,17 @@ function customScriptLoaded() {
 
         // Load subservice lists dynamically
         function loadSubservices() {
+            console.log('Loading subservices.');
             jQuery('.is-area-tag').each(function() {
                 const $this = jQuery(this);
                 const serviceAreaSlug = $this.attr('id').replace('area-', '');
                 const serviceAreaName = jQuery(`#name-${serviceAreaSlug}`).text().trim();
                 const serviceAreaUrl = `/service-area/${serviceAreaSlug}`;
 
+                console.log(`Loading subservices for ${serviceAreaName}.`);
+
                 $this.load(`/service-area/${serviceAreaSlug} .subservice-list`, function() {
+                    console.log(`Subservices loaded for ${serviceAreaName}.`);
                     requestAnimationFrame(() => {
                         handleSingleSubservice();
                         const $collectionList = $this.find('.nav-collection-list');
@@ -110,6 +124,7 @@ function customScriptLoaded() {
         }
 
         // Initialize the dynamic loading and subservice handling on page load
+        console.log('Initializing dynamic subservice loading.');
         debounceLoadSubservices();
     });
 }
