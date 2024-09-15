7fd09347a716d2f23fdb703a914f6228f09d4c59
Ellis Osen <timothy.osen@gmail.com>
2024-09-14 21:58:15 -0500

    changed resource

--- resourcesNav.js
+++ resourcesNav.js
@@ -1,37 +1,48 @@
 (function () {
-    const dynamicList = document.querySelector('.resources-collection-list');
-
-    // Function to debounce and batch DOM updates in requestAnimationFrame
-    function moveStaticItems() {
+    const checkAndRun = function(callback) {
+      if (window.jQuery && typeof Webflow !== 'undefined' && typeof Webflow.require === 'function') {
+        callback();
+      } else {
+        console.log("jQuery or Webflow not ready, retrying...");
+        setTimeout(function() { checkAndRun(callback); }, 100); // Retry after 100ms
+      }
+    };
+  
+    checkAndRun(function() {
+      console.log("jQuery and Webflow are ready, running script.");
+      const dynamicList = document.querySelector('.resources-collection-list');
+  
+      function moveStaticItems() {
         const staticItems = document.querySelectorAll('[fs-cmsstatic-element="static-item"]');
-        
         if (staticItems.length > 0) {
-            const fragment = document.createDocumentFragment();
-            const staticItemsArray = Array.from(staticItems).reverse();
-
-            staticItemsArray.forEach(function(staticItem) {
-                const clonedItem = staticItem.cloneNode(true);
-                const wrapperDiv = document.createElement('div');
-                wrapperDiv.setAttribute('role', 'listitem');
-                wrapperDiv.classList.add('w-dyn-item');
-                wrapperDiv.appendChild(clonedItem);
-                
-                fragment.appendChild(wrapperDiv);
-                staticItem.remove(); // Removing static item in a batch process
-            });
-
-            requestAnimationFrame(() => {
-                dynamicList.insertBefore(fragment, dynamicList.firstChild);
-                Webflow.require('ix2').init(); // Reinitialize Webflow interactions in one go
-            });
+          const fragment = document.createDocumentFragment();
+          const staticItemsArray = Array.from(staticItems).reverse();
+  
+          staticItemsArray.forEach(function(staticItem) {
+            const clonedItem = staticItem.cloneNode(true);
+            const wrapperDiv = document.createElement('div');
+            wrapperDiv.setAttribute('role', 'listitem');
+            wrapperDiv.classList.add('w-dyn-item');
+            wrapperDiv.appendChild(clonedItem);
+  
+            fragment.appendChild(wrapperDiv);
+            staticItem.remove(); // Removing static item in a batch process
+          });
+  
+          requestAnimationFrame(() => {
+            dynamicList.insertBefore(fragment, dynamicList.firstChild);
+            Webflow.require('ix2').init(); // Reinitialize Webflow interactions in one go
+          });
         } else {
-            console.error("No static items found!");
+          console.error("No static items found!");
         }
-    }
-
-    if (dynamicList) {
+      }
+  
+      if (dynamicList) {
         requestAnimationFrame(moveStaticItems);
-    } else {
+      } else {
         console.error("Dynamic list '.resources-collection-list' not found!");
-    }
-})();
+      }
+    });
+  })();
+  
